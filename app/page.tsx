d0bb5e89d17aff8af6d93c545936209c11939f54
Jerry <jerrypaulsam@gmail.com>
2025-12-04 11:09:20 +0530

    ads commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
+import Link from 'next/link';
 import AdUnit from '../components/AdUnit';
 import ClientGameWrapper from '../components/ClientGameWrapper';
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center bg-gray-50 overflow-x-hidden">
-      
+
       {/* Header */}
       <h1 className="text-3xl md:text-5xl font-extrabold my-4 text-green-700 tracking-tight text-center">
         🪰 Fly SWAT!
       </h1>
-      
+      <p style={{ textAlign: "center" }}>Took a break while developing <strong>Shopdibz </strong> marketplace. <br />
+        Check us out: <a href='https://www.shopdibz.com' target='_blank' style={{ textDecoration: "none" }}>Shop High Quality Indian brands</a></p>
+
       {/* Top Banner Ad */}
       <div className="w-full max-w-4xl px-2">
-        <AdUnit slotId="YOUR_TOP_BANNER_ID" />
+        <AdUnit slotId="5417239946" />
       </div>
 
       {/* Main Content Row */}
       <div className="flex flex-col lg:flex-row gap-4 w-full max-w-5xl mt-2 justify-center px-2 md:px-4">
-        
+
         {/* Left Ad (Desktop Only) */}
         <div className="hidden lg:block w-[160px] flex-shrink-0">
-             <AdUnit slotId="YOUR_SKYSCRAPER_ID" format="auto" layout="display" />
+          <AdUnit slotId="1499720824" format="auto" layout="display" />
         </div>
 
         {/* Game Area (Flexible Width) */}
@@ -30,13 +33,18 @@ export default function Home() {
 
         {/* Right Ad (Desktop Only) */}
         <div className="hidden lg:block w-[160px] flex-shrink-0">
-            <AdUnit slotId="YOUR_SKYSCRAPER_ID_2" format="auto" layout="display" />
+          <AdUnit slotId="2020920413" format="auto" layout="display" />
         </div>
       </div>
 
+      <Link href={"https://www.shopdibz.com/download-app"} target='_blank'>
+        <img src="/SwipeBuzz Banner.webp" width={400} style={{ marginBottom: "15px" }} />
+      </Link>
+
+
       {/* Bottom Banner Ad */}
       <div className="w-full max-w-4xl mt-8 px-2">
-        <AdUnit slotId="YOUR_BOTTOM_BANNER_ID" />
+        <AdUnit slotId="1477994933" />
       </div>
     </main>
   );
